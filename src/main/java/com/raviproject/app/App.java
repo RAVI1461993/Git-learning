74e95e2f5bb438498397f4dd6fa57e1a9f47eba0
RAVI1461993 <ravitejamechanical9@gmail.com>
2021-08-21 11:36:13 +0000

    first commit

---
+++ src/main/java/com/raviproject/app/App.java
@@ -0,0 +1,13 @@
+package com.raviproject.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
